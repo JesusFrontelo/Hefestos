a3d9ea8cbcc3d7846cf1b2b559e56a4f25a49988
j.frontelo.gonzalvez@accenture.com <j.frontelo.gonzalvez@accenture.com>
2022-01-25 15:24:14 +0100

    Retired New-ScheduledTaskPrincipal

--- POWERSHELL/Logrotate_Windows.ps1
+++ POWERSHELL/Logrotate_Windows.ps1
@@ -1,14 +1,17 @@
-Write-Host "This script erases older files from a path" -ForegroundColor Green
+Write-Host ""
+Write-Host "This script creates a task scheduler wich erases older files from a path" -ForegroundColor Green
+Write-Host ""
 
-$ruta = Read-Host -Prompt 'Type absolute path to the folder: '
-$older = Read-Host -Prompt 'Specify maximum age of the files to preserve: '
+$ruta = Read-Host -Prompt 'Type absolute path to the folder'
+Write-Host ""
+$older = Read-Host -Prompt 'Specify maximum age, in days, of the files to preserve'
 
-Write-Host "Files older than $older will be deleted from path $ruta"
-Do { $answer = Read-Host  -Prompt "¿Are you sure? (Y/N): " } While ($answer -notmatch "Y|N|y|n")
+Write-Host "Files older than $older days will be deleted from path $ruta"
+Do { $answer = Read-Host  -Prompt "¿Are you sure? (Y/N)" } While ($answer -notmatch "Y|N|y|n")
 
-forfiles /p "$ruta" /s /d -$older /c "cmd /c del @file /Q"
+$taskTrigger = New-ScheduledTaskTrigger -Daily -At 9am
+$taskAction  =  New-ScheduledTaskAction -Execute "C:\Windows\System32\forfiles.exe" -Argument "/p $ruta /s /d -$older /c 'cmd /c del @file /Q\'"
+$taskName = "Log Rotate"
+$description = "This task erase files older than $older days from the path $ruta"
 
-$Trigger = New-ScheduledTaskTrigger -Daily -At 9am
-$Action  =  New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "forfiles /p '$ruta' /s /d -$older /c 'cmd /c del @file /Q'" 
-$Parameters = New-ScheduledTaskPrincipal -RunLevel Highest
-Register-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Parameters -TaskName "Log Rotate" -Description "This task erase files older than $older days from the path $ruta"
+Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName $taskName -Description $description -User "NT AUTHORITY\SYSTEM"
