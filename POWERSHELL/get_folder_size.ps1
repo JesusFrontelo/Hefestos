05327b802a86c34bc14a8b54ab8aa18f43435883
@JesusFrontelo <jesusfrontelo@gmail.com>
2022-02-17 11:31:05 +0100

    Changed columns names

--- POWERSHELL/get_folder_size.ps1
+++ POWERSHELL/get_folder_size.ps1
@@ -1,13 +1,17 @@
+
 $targetfolder=Read-Host -Prompt 'Input path (i.e: D:\ or D:\logs\)'
+
 $dataColl = @()
+
 gci -force $targetfolder -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
-$len = 0
-gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
-$foldername = $_.fullname
-$foldersize= '{0:N2}' -f ($len / 1Gb)
-$dataObject = New-Object PSObject
-Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldername” -value $foldername
-Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldersizeGb” -value $foldersize
-$dataColl += $dataObject
+    $len = 0
+    gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+    $foldername = $_.fullname
+    $foldersize= '{0:N2}' -f ($len / 1Gb)
+    $dataObject = New-Object PSObject
+    Add-Member -inputObject $dataObject -memberType NoteProperty -name “Folder” -value $foldername
+    Add-Member -inputObject $dataObject -memberType NoteProperty -name “Size in GB” -value $foldersize
+    $dataColl += $dataObject
 }
+
 $dataColl | Out-GridView -Title “Size of subdirectories”
