2e279151ae73fa7332fd531ff96d94169ce04986
@JesusFrontelo <jesusfrontelo@gmail.com>
2022-03-30 14:05:56 +0200

    Double quotes corrected

--- POWERSHELL/Logrotate_Windows.ps1
+++ POWERSHELL/Logrotate_Windows.ps1
@@ -10,7 +10,7 @@ Write-Host "Files older than $older days will be deleted from path $ruta"
 Do { $answer = Read-Host  -Prompt "¿Are you sure? (Y/N)" } While ($answer -notmatch "Y|N|y|n")
 
 $taskTrigger = New-ScheduledTaskTrigger -Daily -At 9am
-$taskAction  =  New-ScheduledTaskAction -Execute "C:\Windows\System32\forfiles.exe" -Argument "/p $ruta /s /d -$older /c \"cmd /c del @file /Q\""
+$taskAction  =  New-ScheduledTaskAction -Execute "C:\Windows\System32\forfiles.exe" -Argument "/p $ruta /s /d -$older /c `"cmd /c del @file /Q`""
 $taskName = "Log Rotate"
 $description = "This task erase files older than $older days from the path $ruta"
 
